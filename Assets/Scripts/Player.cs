5816e790564bbec8de9076caec781ee6869a5332
jinseo0517 <jinseo0571@gmail.com>
2025-10-19 17:21:24 +0900

    [feat]플레이어 몸 파츠나눔
    제발 움직여라..

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+
+    Vector3 moveVec;
+
+    private void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+    }
+
+}
